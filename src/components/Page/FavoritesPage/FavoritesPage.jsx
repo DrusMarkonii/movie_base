de541898d26c4d907be8f54c58ae872d4733d013
Makarevich Andrei <andrey8566ok@gmail.com>
2022-04-26 06:28:16 +0400

    add key

---
+++ src/components/Page/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Header from "../../Header/Header";
+import { IMAGE_API_PATH } from "../../../service/endpoints";
+import feature_true from "../../../assets/img/feature_true.png";
+
+import "./FavoritesPage.scss";
+
+function FavoritesPage() {
+  const [isAdded, setIsAdded] = useState(false);
+  const [favoriteFilms, setFavoriteFilms] = useState([]);
+
+  useEffect(() => {
+    let localStorageFilms = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavoriteFilms(localStorageFilms);
+  }, []);
+
+  const removeFromFavorites = (id) => {
+    let removedFilm = favoriteFilms.filter((film) => film.id !== id);
+    localStorage.setItem("favorites", JSON.stringify(removedFilm));
+    setFavoriteFilms(removedFilm);
+  };
+
+  
+  return (
+    <div className="favoritesPage">
+      <Header />
+      {favoriteFilms.length ? (
+        <div className="favoriteFilms">
+          {favoriteFilms.map(
+            ({ poster_path, original_title, id, vote_average }) => (
+              <div className="favoriteCard" key={original_title}>
+                <Link
+                  to={`/film/${id}`}
+                  className="filmCard_card_link"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <img
+                    className="filmCard_card_img"
+                    src={`${IMAGE_API_PATH}${poster_path}`}
+                    alt={original_title}
+                    onClick={() => setIsAdded(!isAdded)}
+                  />
+                  <div className="filmCard_description_box">
+                    <span className="filmCard_description_title">
+                      {original_title}
+                    </span>
+                    <img
+                      src={feature_true}
+                      alt="rate"
+                      width={15}
+                      style={{ marginLeft: 10 }}
+                    />
+                    <span className="filmCard_description_rate">
+                      {vote_average}
+                    </span>
+                  </div>
+                </Link>
+                <div className="filmCard_favorites">
+                  <img
+                    src={feature_true}
+                    alt="feature_true"
+                    className="favoriteCard__feature_true"
+                    onClick={() => {
+                      removeFromFavorites(id);
+                    }}
+                  />
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      ) : (
+        <div className="favorites_message">
+          You can add the movie to your favorites...
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FavoritesPage;
---
+++ src/components/Page/FilmPage/FilmPage.jsx
@@ -0,0 +1,183 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import {
+  getFilmOfDescription,
+  getRecommendedFilms,
+  getTrailerOfFilm,
+  IMAGE_API_PATH,
+} from "../../../service/endpoints";
+import {
+  addToFavoritesAction,
+  removeFromFavoritesAction,
+} from "../../../store/action-creators/filmsActions";
+import FilmCard from "../../FilmCard/FilmCard";
+import Header from "../../Header/Header";
+import Spinner from "../../Spinner/Spinner";
+import feature_false from "../../../assets/img/feature_false.png";
+import feature_true from "../../../assets/img/feature_true.png";
+
+import "./FilmPage.scss";
+
+function FilmPage() {
+  const [film, setFilm] = useState(null);
+  const [recommended, setRecommended] = useState(null);
+  const [idOfFilm, setIdOfFilm] = useState(null);
+  const [isAdded, setIsAdded] = useState(false);
+  const [video, setVideo] = useState("");
+  const [favoriteFilms, setFavoriteFilms] = useState([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIdOfFilm(window.location.pathname.split("/")[2]);
+    console.log(idOfFilm)
+  }, []);
+
+  useEffect(() => {
+    if (idOfFilm) {
+      filmOfDescription(idOfFilm);
+      recommendedFilms(idOfFilm);
+    }
+  }, [idOfFilm]);
+
+  useEffect(() => {
+    let localStorageFilms = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavoriteFilms(localStorageFilms);
+  }, []);
+
+  const filmOfDescription = async (id) => {
+    const film = await getFilmOfDescription(id);
+    const video = await getTrailerOfFilm(id);
+    if (video) {
+      setFilm(() => film);
+      setVideo(() => video.results[0].key);
+    }
+  };
+
+  const addToFavorites = () => {
+    setIsAdded(!isAdded);
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    dispatch(addToFavoritesAction(film));
+    favorites.push(film);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
+
+  const removeFromFavorites = (id) => {
+    setIsAdded(!isAdded);
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    dispatch(removeFromFavoritesAction(id));
+    favorites = favorites.filter((film) => film.id !== id);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
+
+  const recommendedFilms = async (id) => {
+    const recommended = await getRecommendedFilms(id);
+    setRecommended(recommended);
+  };
+
+  return (
+    <>
+      <Header />
+      {film && idOfFilm && recommended ? (
+        <div className="filmPage">
+          <div className="filmOfDescription">
+            <div className="filmOfDescription__imageBox">
+              <img
+                className="filmPage_img"
+                src={`${IMAGE_API_PATH}${film.poster_path}`}
+                alt={film.title}
+              />
+            </div>
+            <div className="favoritesBtn">
+              {isAdded ? (
+                <img
+                  src={feature_true}
+                  alt="feature_true"
+                  className="feature_true"
+                  onClick={() => {
+                    removeFromFavorites(film.id);
+                  }}
+                />
+              ) : (
+                <img
+                  src={feature_false}
+                  alt="feature_false"
+                  className="feature_false"
+                  onClick={() => {
+                    addToFavorites(film.id);
+                  }}
+                />
+              )}
+            </div>
+            <div className="filmOfDescription__descriptionBox">
+              <h1 className="filmTitle">{film.title}</h1>
+              <div className="dataBox">
+                <p className="dataBox__item">
+                  <b>Country:</b> <i>{film.production_countries[0].name}</i>
+                </p>
+                <p className="dataBox__item">
+                  <b>Runtime:</b> <i>{film.runtime}</i>
+                </p>
+                <p className="dataBox__item">
+                  <b>Genre:</b>{" "}
+                  {film.genres.map((item) => (
+                    <i key={item.id}> {item.name}</i>
+                  ))}
+                </p>
+                <p className="dataBox__item">
+                  <b>Release:</b> <i>{film.release_date}</i>
+                </p>
+                <p className="dataBox__item">
+                  <b>Rating:</b> <i>{film.vote_count}</i>
+                </p>
+                <p className="dataBox__item">
+                  <b>Description:</b> <i> {film.overview}</i>
+                </p>
+              </div>
+              <div className="VideoBox">
+                <iframe
+                  src={`https://www.youtube.com/embed/${video}`}
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+                  title="video"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="recommendedBox">
+            <p className="titleOfRecommendation">Recommended:</p>
+            <div className="recommendedBox_list">
+              {recommended.results.map(
+                ({
+                  id,
+                  original_title,
+                  overview,
+                  poster_path,
+                  original_language,
+                  vote_average,
+                  genre_ids,
+                }) => (
+                  <FilmCard
+                    key={id}
+                    original_title={original_title}
+                    overview={overview}
+                    poster_path={poster_path}
+                    original_language={original_language}
+                    vote_average={vote_average}
+                    genre_ids={genre_ids}
+                  />
+                )
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Spinner />
+      )}
+    </>
+  );
+}
+
+export default FilmPage;
